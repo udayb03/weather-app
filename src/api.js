5fad9f060ebcb26b6a4e64852ee64ec64b4b177f
Uday Baldha <udaybaldha310@gmail.com>
2024-06-10 14:58:13 +0530

    commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "25c14634ecmsh9f2a7744bc9b8f7p1d7c6bjsn6a3d10eff599", 
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "40fe349848db71fc599e739c5d255607"; 
